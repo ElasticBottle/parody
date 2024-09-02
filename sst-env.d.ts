c7d3248004b32ffa6d6eb56dd9086021baca7db8
Winston Yeo <winstonyeo99@yahoo.com>
2024-09-03 01:06:33 +0800

    fix: type error for infra

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,30 +3,6 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    "Api": {
-      "type": "sst.cloudflare.Worker"
-      "url": string
-    }
-    "StorageBucket": {
-      "type": "sst.cloudflare.Bucket"
-    }
-    "StripePublic": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "StripeSecret": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "StripeWebhook": {
-      "id": string
-      "secret": string
-      "type": "stripe.index/webhookEndpoint.WebhookEndpoint"
-    }
-    "WWW": {
-      "type": "sst.cloudflare.StaticSite"
-      "url": string
-    }
   }
 }
 export {}
