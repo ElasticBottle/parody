5b2f866437b1894501364f2470bed6e0f262af3d
Winston Yeo <winstonyeo99@yahoo.com>
2024-10-01 21:27:44 +0800

    chore: fix formatting for core package

--- packages/core/src/image/file-upload-schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const FileUploadSchema = z.object({
-	images: z.array(z.instanceof(File)).min(5, "Please upload at least 5 images"),
-});
-export type FileUploadSchemaType = z.infer<typeof FileUploadSchema>;
--- packages/core/src/random/generate-id.ts
+++ packages/core/src/random/generate-id.ts
@@ -1,4 +1,4 @@
 export const generateId = (length = 32) => {
-	const buffer = crypto.getRandomValues(new Uint8Array(length));
-	return Buffer.from(buffer).toString("base64");
+  const buffer = crypto.getRandomValues(new Uint8Array(length));
+  return Buffer.from(buffer).toString("base64");
 };
