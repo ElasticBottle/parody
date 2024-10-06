f5a103154e8452a047ccb56941cb0020d5940325
Winston Yeo <winstonyeo99@yahoo.com>
2024-10-07 06:13:22 +0800

    chore: remove unused stuff

--- packages/core/src/error/get-error-string.ts
+++
@@ -1,8 +0,0 @@
-export function getErrorString(err: unknown) {
-  const unknownError = "Something went wrong, please try again later.";
-
-  if (err instanceof Error) {
-    return err.message;
-  }
-  return unknownError;
-}
