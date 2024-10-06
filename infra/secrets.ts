3aa1fc49b0e9fc9a8c05758b38958a0ff67512c1
Winston Yeo <winstonyeo99@yahoo.com>
2024-10-07 06:16:15 +0800

    chore: update lockfile fix: formatting

--- infra/secrets.ts
+++ infra/secrets.ts
@@ -1,6 +1,6 @@
 export const secret = {
-	StripeSecret: new sst.Secret("StripeSecret", process.env.STRIPE_API_KEY),
-	StripePublic: new sst.Secret("StripePublic", process.env.STRIPE_PUBLIC_KEY),
+  StripeSecret: new sst.Secret("StripeSecret", process.env.STRIPE_API_KEY),
+  StripePublic: new sst.Secret("StripePublic", process.env.STRIPE_PUBLIC_KEY),
 };
 
 export const allSecrets = Object.values(secret);
